7a6f94514e1565d17eaf05d47e21ce4d80b3d5dc
hernan findlay <hernanfindlay@hotmail.com.ar>
2023-10-25 21:09:44 -0300

    proyecto-final

--- main.js
+++ main.js
@@ -1,77 +1,64 @@
+const actividades = [
+  { id: 1, act: "Evento", valor: 5000 },
+  { id: 2, act: "Degustación", valor: 8000 },
+  { id: 3, act: "Visita", valor: 4000 }
+];
 
-if (localStorage.getItem("productos")) {  //anda al localstorage y traeme la key productos
-  lista = JSON.parse(localStorage.getItem("productos")); //parseame todo y metelo en la lista
-  } else {
-  lista = lista  //si no hay nada, la lista es igual a la lista de siempre
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("ape");
+const email = document.getElementById("mail");
+const tel = document.getElementById("cel");
+const acti = document.getElementById("act");
+const acti2 = document.getElementById("act2");
+const cant = document.getElementById("cant");
+const pagar = document.getElementById("pagar");
+
+
+const usuario = JSON.parse(localStorage.getItem("usuario")) || {};
+  nombre.value = usuario.nombre || "";
+  apellido.value = usuario.apellido || "";
+  email.value = usuario.email || "";
+  tel.value = usuario.tel || "";
+
+
+actualizarCostoTotal();
+
+document.getElementById("boton").addEventListener("click", function () {
+ 
+  const usuario = {
+    nombre: nombre.value,
+    apellido: apellido.value,
+    email: email.value,
+    telefono: tel.value
+  };
+  localStorage.setItem("usuario", JSON.stringify(usuario));
+
+  actualizarCostoTotal();
+});
+
+function actualizarCostoTotal() {
+  const sel = acti.value;
+  const sel2 = acti2.value;
+  const cantidadPersonas = cant.value;
+
+  let costoTotal = 0;
+
+  if (!isNaN(sel) && sel >= 1 && sel <= 3) {
+    costoTotal += actividades.find((act) => act.id === parseInt(sel)).valor;
   }
 
-recuperarDatos()
-
-let cel = document.getElementById("cel")
-let cantidad = document.getElementById("cant")
-let boton = document.getElementById("boton")
-
-
-function usuario(){
-
-  let nombre = document.getElementById("nombre").toUpperCase()
-  let apellido = document.getElementById("ape").toUpperCase()
-  let mail= document.getElementById("mail")
-
-  if ((nombre === nombre.value)&&(apellido===apellido.value)){
-
-    console.log("usuario ya registrado") 
-
-  }else{
-    localStorage.setItem("nombre", nombre).toUpperCase()
-    localStorage.setItem("apellido", apellido).toUpperCase()
-    localStorage.setItem("email", mail)
+  if (!isNaN(sel2) && sel2 >= 1 && sel2 <= 3) {
+    costoTotal += actividades.find((act) => act.id === parseInt(sel2)).valor;
   }
 
+  if (!isNaN(cantidadPersonas) && cantidadPersonas > 0 && cantidadPersonas <= 6) {
+    costoTotal *= cantidadPersonas;
+  }
+
+  pagar.value = costoTotal;
+
+ 
 }
-usuario()
-
-const actividad = [{id:1, acti:"eventos", valor: 500  },
-                    {id:2 , acti:"visita", valor:250},
-                    {id:3 , acti:"degustacion" , valor:350},]
-
-
-let act = document.getElementById("act")
-let act2 = document.getElementById("act2")
-
-function cobroact (){ 
-  let a1 = function(){
-
-    if ((act == 1 )||( act == 2 )||( act == 3)){
-    actividad.forEach(id)
-    }
-
-}
-}
-
-cobroact()
-
-
-
-function recuperarDatos(){
-  nombre.value = localStorage.getItem("nombre"),
-  apellido.value = localStorage.getItem("apellido"),
-  act.value = localStorage.getItem("actividad")
-  cantidad.value = localStorage.getItem("cant")
-  email.value = localStorage.getItem("mail")
-}
-
-/* const guardarusuario = function (){
-
-                        
-                        localStorage.setItem("actividad", actividad);
-                        localStorage.setItem("cant", cantidad );
-
-
-
-} */
-                    
-
 
 
 
